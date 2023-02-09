c8102dc67f1f938247de08231d018e664776d833
Michael Fester <michael.fester@gmail.com>
2023-02-09 15:25:27 -0800

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,14 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-const docsHostname = 'https://commercial-mollusk-scarlet.motif.land'
-
 export default async function middleware(req: NextRequest) {
-  // const { pathname } = req.nextUrl
+  const { pathname } = req.nextUrl
 
-  // if (pathname.startsWith('/docs')) {
-  //   return NextResponse.rewrite(docsHostname + pathname)
-  // } else if (pathname.startsWith('/__motif/assets/_next/')) {
-  //   return NextResponse.rewrite(docsHostname + pathname)
-  // }
+  if (pathname.startsWith('/__motif/assets/_next/')) {
+    const url = req.nextUrl.clone()
+    url.pathname = pathname.replace(/^\/__motif\/assets/, '')
+    return NextResponse.rewrite(url)
+  }
 
   return NextResponse.next()
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: '/__motif/assets',
 }
 
 module.exports = nextConfig

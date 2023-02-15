7cfd753b1d0fa40f97281e0fb0ff54a7a5b73df1
Michael Fester <michael.fester@gmail.com>
2023-02-14 21:28:35 -0800

    Add experimental flag

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/__motif/assets',
   skipMiddlewareUrlNormalize: true,
+  experimental: true,
 }
 
 module.exports = nextConfig

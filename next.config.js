ebcee50792a6ee77c00390ba60cadb081a35b906
Michael Fester <michael.fester@gmail.com>
2023-02-14 22:14:25 -0800

    Wip

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   skipMiddlewareUrlNormalize: true,
+  experimental: {
+    externalMiddlewareRewritesResolve: true,
+  },
 }
 
 module.exports = nextConfig

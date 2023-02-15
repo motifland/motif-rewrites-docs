ee9556d407e2230bba34275ff989e936ffb0e409
Michael Fester <michael.fester@gmail.com>
2023-02-14 22:34:06 -0800

    Wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,30 @@
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div
+        style={{
+          width: 200,
+          height: '100vh',
+          position: 'fixed',
+          overflowY: 'auto',
+        }}
+      >
+        {Array.from(Array(100).keys()).map((i) => (
+          <div key={i}>
+            <Link href={`/docs/page${(i % 2) + 1}`}>Page {(i % 2) + 1}</Link>
+          </div>
+        ))}
+      </div>
+      <div
+        style={{
+          paddingLeft: 200,
+        }}
+      >
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
--- pages/docs/page2.tsx
+++ pages/docs/page2.tsx
@@ -3,18 +3,11 @@ import Link from 'next/link'
 export default function Page2() {
   return (
     <>
-      <main>
-        <div>
-          <Link href="/docs/page1">Page 1</Link>
-        </div>
-        <div>
-          <Link href="/docs/page2">Page 2</Link>
-        </div>
-        <div>This is the rewritten app</div>
-        <div>
-          <Link href="/">Back to host app</Link>
-        </div>
-      </main>
+      <div>Page 2</div>
+      <div>This is the rewritten app</div>
+      <div>
+        <Link href="/">Back to host app</Link>
+      </div>
     </>
   )
 }

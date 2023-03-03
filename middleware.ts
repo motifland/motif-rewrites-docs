66a5e56a40844aa85bac7a26d9254f4ecd93d6e4
Michael Fester <michael.fester@gmail.com>
2023-03-03 11:39:47 -0800

    Wip

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,33 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const { pathname } = req.nextUrl
 
+  console.log('pathname', pathname)
   if (pathname.startsWith('/__motif/assets/_next/')) {
     const url = req.nextUrl.clone()
     url.pathname = pathname.replace(/^\/__motif\/assets/, '')
     return NextResponse.rewrite(url)
   }
+
+  // if (pathname.startsWith('/_next/data/development/')) {
+  //   const url = req.nextUrl.clone()
+  //   url.pathname = pathname
+  //     .replace('/_next/data/development/', '/')
+  //     .replace(/\.json$/, '')
+  //   console.log(
+  //     'Replace',
+  //     pathname.replace('/_next/data/development/', '/').replace(/\.json$/, '')
+  //   )
+  //   return NextResponse.rewrite(url)
+  // }
+
+  // console.log('pathname', pathname)
+  // if (pathname.startsWith('/docs')) {
+  //   const path = `/domains/test${pathname}`
+  //   const url = req.nextUrl.clone()
+  //   url.pathname = path
+  //   console.log('Rewrite to', path)
+  //   return NextResponse.rewrite(url)
+  // }
 
   return NextResponse.next()
 }

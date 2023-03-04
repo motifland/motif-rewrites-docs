4e20a860e2e52c13a07ca9161f44e9460072adcc
Michael Fester <michael.fester@gmail.com>
2023-03-03 19:28:15 -0800

    Wip

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,40 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
+// export const config = {
+//   // matcher: ['/((?!api|_next|static|favicon.ico|site.webmanifest).*)'],
+//   matcher: ['/((?!api|_next|static|favicon.ico|site.webmanifest).*)'],
+// }
+
 export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const { pathname } = req.nextUrl
 
-  console.log('pathname', pathname)
-  if (pathname.startsWith('/__motif/assets/_next/')) {
-    const url = req.nextUrl.clone()
-    url.pathname = pathname.replace(/^\/__motif\/assets/, '')
-    return NextResponse.rewrite(url)
-  }
+  // if (pathname.startsWith('/__motif/assets/_next/')) {
+  //   const url = req.nextUrl.clone()
+  //   url.pathname = pathname.replace(/^\/__motif\/assets/, '')
+  //   return NextResponse.rewrite(url)
+  // }
+
+  // console.log('pathname', pathname)
+  // if (pathname.startsWith('/__motif/assets/_next/')) {
+  //   const url = req.nextUrl.clone()
+  //   url.pathname = pathname.replace(/^\/__motif\/assets/, '')
+  //   return NextResponse.rewrite(url)
+  // }
+
+  // if (pathname.startsWith('/_next/data/')) {
+  //   // Input: /_next/data/development/docs/hi.json
+  //   // Output: /_next/data/development/_projectdomain/joy-kin-arc/docs/hi.json
+  //   const url = req.nextUrl.clone()
+  //   const domainPath = 'domain'
+  //   const parts = pathname.split('/')
+  //   url.pathname = [
+  //     ...parts.slice(0, 4),
+  //     domainPath,
+  //     'test',
+  //     ...parts.slice(4),
+  //   ].join('/')
+  //   return NextResponse.rewrite(url)
+  // }
 
   // if (pathname.startsWith('/_next/data/development/')) {
   //   const url = req.nextUrl.clone()
@@ -22,11 +48,9 @@ export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
   //   return NextResponse.rewrite(url)
   // }
 
-  // console.log('pathname', pathname)
   if (pathname.startsWith('/docs')) {
-    const path = `/domains/test${pathname}`
     const url = req.nextUrl.clone()
-    url.pathname = path
+    url.pathname = `/domains/test${pathname}`
     return NextResponse.rewrite(url)
   }
 
--- pages/domains/[domain]/docs/page2.tsx
+++ pages/domains/[domain]/docs/page2.tsx
@@ -6,6 +6,7 @@ export default function Page2() {
       <div>Page 2</div>
       <div>This is the rewritten app</div>
       <div>
+        <Link href="/docs/page1">Page 1</Link>
         <Link href="/">Back to host app</Link>
       </div>
     </>

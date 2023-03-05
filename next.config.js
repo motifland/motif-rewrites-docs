589c7d60decbf951c1f38bd919cb3c446e263e7d
Michael Fester <michael.fester@gmail.com>
2023-03-05 14:52:02 -0800

    Add logs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 // Attempt 1
+// This is with regular paths which are not rewritten.
 // - Keep skipMiddlewareUrlNormalize
 // - Add this externalMiddlewareRewritesResolve here
 // This WORKS!
-// ---------------------------------------------------
 // Attempt 2
+// This is with regular paths which are not rewritten.
+// - Remove skipMiddlewareUrlNormalize
+// - Keep externalMiddlewareRewritesResolve
+// This WORKS!
+// ---------------------------------------------------
+// Attempt 3
 // Now we have middleware rewrites /docs/... to
 // /domains/.../docs./...
 // - Keep externalMiddlewareRewritesResolve here
@@ -13,7 +19,7 @@
 const nextConfig = {
   assetPrefix: '/__motif/assets',
   reactStrictMode: true,
-  skipMiddlewareUrlNormalize: true,
+  // skipMiddlewareUrlNormalize: true,
   experimental: {
     externalMiddlewareRewritesResolve: true,
   },

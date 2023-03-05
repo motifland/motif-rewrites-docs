fc67298f9b05832feea3f710c9855b3f9c4fdf80
Michael Fester <michael.fester@gmail.com>
2023-03-05 13:44:26 -0800

    Wip

--- pages/with-getstaticprops/[...slug].tsx
+++ pages/with-getstaticprops/[...slug].tsx
@@ -3,24 +3,34 @@ import { useRouter } from 'next/router'
 import { ParsedUrlQuery } from 'querystring'
 import React from 'react'
 
-export const getStaticProps: GetStaticProps<{
-  params: ParsedUrlQuery | undefined
-}> = async ({ params }) => {
-  return { props: { params } }
-}
+// export const getStaticProps: GetStaticProps<{
+//   params: ParsedUrlQuery | undefined
+// }> = async ({ params }) => {
+//   return { props: { params } }
+// }
 
-export const getStaticPaths = async () => {
-  return { paths: [], fallback: 'blocking' }
-}
+// export const getStaticPaths = async () => {
+//   return { paths: [], fallback: 'blocking' }
+// }
 
-export default function Slug({
-  params,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+// export default function Slug({
+//   params,
+// }: InferGetStaticPropsType<typeof getStaticProps>) {
+//   const router = useRouter()
+
+//   return (
+//     <>
+//       <div>Params: {JSON.stringify(params)}</div>
+//     </>
+//   )
+// }
+
+export default function Slug() {
   const router = useRouter()
 
   return (
     <>
-      <div>Params: {JSON.stringify(params)}</div>
+      <div>Query: {JSON.stringify(router.query)}</div>
     </>
   )
 }

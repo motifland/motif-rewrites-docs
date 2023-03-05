fb983808ed18e003d1e0d07ec66ecf1c938249f4
Michael Fester <michael.fester@gmail.com>
2023-03-05 15:03:52 -0800

    Add logs

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@
 const nextConfig = {
   assetPrefix: '/__motif/assets',
   reactStrictMode: true,
-  // skipMiddlewareUrlNormalize: true,
+  skipMiddlewareUrlNormalize: true,
   experimental: {
     externalMiddlewareRewritesResolve: true,
   },
--- pages/domains/[domain]/docs/[...slug].tsx
+++ pages/domains/[domain]/docs/[...slug].tsx
@@ -3,35 +3,35 @@ import { useRouter } from 'next/router'
 import { ParsedUrlQuery } from 'querystring'
 import React from 'react'
 
-// export const getStaticProps: GetStaticProps<{
-//   params: ParsedUrlQuery | undefined
-// }> = async ({ params }) => {
-//   return { props: { params } }
-// }
+export const getStaticProps: GetStaticProps<{
+  params: ParsedUrlQuery | undefined
+}> = async ({ params }) => {
+  return { props: { params } }
+}
 
-// export const getStaticPaths = async () => {
-//   return { paths: [], fallback: 'blocking' }
-// }
+export const getStaticPaths = async () => {
+  return { paths: [], fallback: 'blocking' }
+}
 
-// export default function Slug({
-//   params,
-// }: InferGetStaticPropsType<typeof getStaticProps>) {
-//   const router = useRouter()
-
-//   return (
-//     <>
-//       <div>Params: {JSON.stringify(params)}</div>
-//     </>
-//   )
-// }
-
-export default function Slug() {
+export default function Slug({
+  params,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter()
 
   return (
     <>
-      No static props
-      <div>Query: {JSON.stringify(router.query)}</div>
+      <div>Params: {JSON.stringify(params)}</div>
     </>
   )
 }
+
+// export default function Slug() {
+//   const router = useRouter()
+
+//   return (
+//     <>
+//       No static props
+//       <div>Query: {JSON.stringify(router.query)}</div>
+//     </>
+//   )
+// }

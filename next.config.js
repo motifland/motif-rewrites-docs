2647f606c43d72400f86ff3509f5301910379c1d
Michael Fester <michael.fester@gmail.com>
2023-03-05 12:49:26 -0800

    Wip

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Cf /docs/middleware.md
-  assetPrefix: '/__motif/assets',
-  // Cf /docs/middleware.md
-  // skipMiddlewareUrlNormalize: true,
-  images: {
-    domains: [
-      'source.unsplash.com',
-      'images.unsplash.com',
-      'res.cloudinary.com',
-    ],
-    formats: ['image/avif', 'image/webp'],
-  },
-  experimental: {
-    externalDir: true, // allows us to build the project with external TS files
-    // externalMiddlewareRewritesResolve: true,
-  },
-  // assetPrefix: '/__motif/assets',
   // reactStrictMode: true,
+  // swcMinify: true,
+  // // Cf /docs/middleware.md
+  // assetPrefix: '/__motif/assets',
+  // // Cf /docs/middleware.md
   // // skipMiddlewareUrlNormalize: true,
+  // images: {
+  //   domains: [
+  //     'source.unsplash.com',
+  //     'images.unsplash.com',
+  //     'res.cloudinary.com',
+  //   ],
+  //   formats: ['image/avif', 'image/webp'],
+  // },
+  // experimental: {
+  //   externalDir: true, // allows us to build the project with external TS files
+  //   // externalMiddlewareRewritesResolve: true,
+  // },
+  assetPrefix: '/__motif/assets',
+  reactStrictMode: true,
+  // skipMiddlewareUrlNormalize: true,
 }
 
 module.exports = nextConfig

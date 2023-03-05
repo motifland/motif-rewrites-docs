4d0c6a383b0767083d4c42a88bc1b9fb686a1688
Michael Fester <michael.fester@gmail.com>
2023-03-05 14:43:51 -0800

    WIP

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@
 const nextConfig = {
   assetPrefix: '/__motif/assets',
   reactStrictMode: true,
-  // skipMiddlewareUrlNormalize: true,
+  skipMiddlewareUrlNormalize: true,
   experimental: {
     externalMiddlewareRewritesResolve: true,
   },

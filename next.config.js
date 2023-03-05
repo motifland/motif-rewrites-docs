49c2fdfaa8a5561e9bcc452c5dbe8d1dd859d95e
Michael Fester <michael.fester@gmail.com>
2023-03-05 13:52:50 -0800

    Wip

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+// Attempt 1
+// - Add this externalMiddlewareRewritesResolve here
+// - Keep skipMiddlewareUrlNormalize
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: '/__motif/assets',
   reactStrictMode: true,
   skipMiddlewareUrlNormalize: true,
+  experimental: {
+    externalMiddlewareRewritesResolve: true,
+  },
 }
 
 module.exports = nextConfig

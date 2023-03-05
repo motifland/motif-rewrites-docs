96f6a8353d708361368b3ad449eba25e5f97aec4
Michael Fester <michael.fester@gmail.com>
2023-03-05 14:32:14 -0800

    WIP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 // Attempt 1
-// - Add this externalMiddlewareRewritesResolve here
 // - Keep skipMiddlewareUrlNormalize
+// - Add this externalMiddlewareRewritesResolve here
+// This WORKS!
+// ---------------------------------------------------
+// Attempt 2
+// - Keep externalMiddlewareRewritesResolve here
+// - Remove skipMiddlewareUrlNormalize
 // This WORKS!
 /** @type {import('next').NextConfig} */
 const nextConfig = {
